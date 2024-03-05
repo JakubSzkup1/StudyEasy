1efcb9dc077619ce0ade9f0fe1b9e78595891891
KyleKeenanGMIT <kylekeenan@live.co.uk>
2024-03-05 19:59:20 +0000

    default routing and routing to homepage with calendar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
+import { HomepageComponent } from './pages/homepage/homepage.component'; // Adjust the path as per your project structure
+
 
 const routes: Routes = [
   {
@@ -20,10 +22,16 @@ const routes: Routes = [
     component: RegistrationComponent
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'home', // Path for the homepage
+    component: HomepageComponent // Specify the HomepageComponent for this route
   },
+
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+
+  
 ];
 
 @NgModule({
