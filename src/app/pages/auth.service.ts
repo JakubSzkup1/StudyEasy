cdf1c657377356ead86b92e117db4df9e611b1c7
JakubSzkup1 <91326317+JakubSzkup1@users.noreply.github.com>
2024-03-20 12:12:25 +0000

    added basic auth service to login

---
+++ src/app/pages/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  //Dummy method while firebase authentication gets sorted
+  authenticate(username: string, password: string): boolean{
+    if(username=== 'admin' && password === 'password'){
+      //Authentication succesful
+      return true;
+    }else {
+      //Authentication failed
+      return false;
+    }
+
+  }
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 //import { AngularFireAuth } from '@angular/fire/auth';
 
 
@@ -15,7 +17,9 @@ export class LoginComponent  implements OnInit {
   passwordControl:FormControl = new FormControl('', Validators.required) //need to work on username and password validator instead of having separate!!!!
   usernameControl: FormControl = new FormControl('', Validators.required);
 
-  constructor() { }
+  errorMessage: string ='';
+
+  constructor(private authService: AuthService,private router: Router) { }
 
   ngOnInit() {
    
@@ -24,8 +28,19 @@ export class LoginComponent  implements OnInit {
   //method to handle login
   login(){
     console.log('Login button clicked');
-  }
 
- 
+    const unsername = this.usernameControl.value;
+    const password = this.passwordControl.value;
+
+    //now call the authentication service to validate the credentials
+    const isAuthenticated= this.authService.authenticate(unsername,password);
+    
+    if(isAuthenticated){
+      this.router.navigate(['home']); //if succesful navigate to the homepage
+    }else{
+      this.errorMessage='Invalid username and password'; //if authentication failed display the error message
+    }
+
+  }
 
 }
