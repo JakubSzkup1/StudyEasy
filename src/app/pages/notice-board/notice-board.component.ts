812dcb4355764dd1b89e53e548cb6c5218b3f387
JakubSzkup1 <91326317+JakubSzkup1@users.noreply.github.com>
2024-04-06 12:35:13 +0000

    Notice board updated & styled

--- src/app/pages/notice-board/notice-board.component.ts
+++ src/app/pages/notice-board/notice-board.component.ts
@@ -5,23 +5,29 @@ import { Notice, NoticeService } from 'src/app/services/notice.service';
 @Component({
   selector: 'app-notice-board',
   templateUrl: './notice-board.component.html',
-})
-export class NoticeBoardComponent implements OnInit {
-  notices: Notice[] = [];
-  newNotice: Notice = { title: '', message: '' };
+  styleUrls: ['./notice-board.component.scss']
 
+})
+ // Notice board component is repsonisbly for dipslaying and adding notices which get stored in firebase
+export class NoticeBoardComponent implements OnInit {
+  notices: Notice[] = []; //Object for the new notices
+  newNotice: Notice = { title: '', message: '' }; // Empty notice 
+
+  //Constructor for the NoticeService dependency
   constructor(private noticeService: NoticeService) {}
 
+  //Component initialization happens here
   ngOnInit(): void {
-    this.noticeService.getNotices().subscribe(notices => {
-      this.notices = notices;
+    this.noticeService.getNotices().subscribe(notices => { //Retrieves notices from the NoticeService
+      this.notices = notices; // and assigns to the array
     });
   }
 
+  //Function to add new notice to the notice board
   addNotice() {
-    if (this.newNotice.title && this.newNotice.message) {
-      this.noticeService.addNotice(this.newNotice).then(() => {
-        // Clear the form
+    if (this.newNotice.title && this.newNotice.message) { //Check if new notice has both title and message
+      this.noticeService.addNotice(this.newNotice).then(() => { //Calls method 'AddNotice' to save new noticee
+        // Clear the form after notice is added
         this.newNotice = { title: '', message: '' };
       });
     }
--- src/app/services/notice.service.ts
+++ src/app/services/notice.service.ts
@@ -8,20 +8,23 @@ export interface Notice {
   message: string;
 }
 
+//This is where all the firebase backend happens for retrieving and getting data
 @Injectable({
   providedIn: 'root',
 })
 export class NoticeService {
   constructor(private firestore: AngularFirestore) {}
 
+  //Add notices to firebase
   addNotice(notice: Notice) {
     return this.firestore.collection('notices').add(notice);
   }
 
+  //Get Notices from firebase
   getNotices(): Observable<Notice[]> {
     return this.firestore.collection<Notice>('notices').valueChanges({ idField: 'id' });
   }
 
-  // more methods for updating will be added here
+  // more methods for updating will be added here (updating & deleting etc.)
   
 }
