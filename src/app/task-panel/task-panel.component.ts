eb00e4229e693e74ceb53960055510fdda7a3eef
JakubSzkup1 <91326317+JakubSzkup1@users.noreply.github.com>
2024-04-23 16:53:22 +0000

    task now store in session storage & delete button added

--- src/app/task-panel/task-panel.component.ts
+++ src/app/task-panel/task-panel.component.ts
@@ -9,6 +9,16 @@ export class TaskPanelComponent {
   newTask: string = ''; //Declare newTask property
   tasks: { name: string, completed: boolean }[] = []; //Declare tasks property
 
+ngOnInit()
+{
+
+  //once component initliazis, load tasks from session storage 
+  const storedTasks = sessionStorage.getItem('tasks');
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+    }
+}
+
   //Method to add new task to the array
   addTask() {
     //Check if input is not empty - 'trim' make sure there is no whitespaces
@@ -16,6 +26,10 @@ export class TaskPanelComponent {
       //Push new task into array with the name of the new task and its completion status set to false
       this.tasks.push({ name: this.newTask, completed: false });
       this.newTask = ''; //Reset newTask after adding task
+      this.saveTasksToSessionStorage();
+
+      //Save the tasks to session storage after adding a new task
+      sessionStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
   }
 
@@ -23,5 +37,18 @@ export class TaskPanelComponent {
   taskCompleted(event: any, task: {name: string, completed: boolean}){
     //updates the status of text based on the checkbox 
     task.completed=event.target.checked;
+    this.saveTasksToSessionStorage();
   }
-}
+
+    //Save tasks to session storage after marking tasks
+    private saveTasksToSessionStorage(){
+      sessionStorage.setItem('tasks', JSON.stringify(this.tasks));
+    } 
+
+    //Method to save task to session storage.
+    deleteTask(index: number) {
+      this.tasks.splice(index, 1);
+      this.saveTasksToSessionStorage();
+    }
+  }
+
