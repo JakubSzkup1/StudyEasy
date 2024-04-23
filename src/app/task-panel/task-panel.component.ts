bb5205a9ca53d90440acc04328f6d4b769321871
JakubSzkup1 <91326317+JakubSzkup1@users.noreply.github.com>
2024-04-23 16:20:14 +0000

    added checkboxes to the task panel

--- src/app/task-panel/task-panel.component.ts
+++ src/app/task-panel/task-panel.component.ts
@@ -7,12 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskPanelComponent {
   newTask: string = ''; //Declare newTask property
-  tasks: string[] = []; //Declare tasks property
+  tasks: { name: string, completed: boolean }[] = []; //Declare tasks property
 
+  //Method to add new task to the array
   addTask() {
+    //Check if input is not empty - 'trim' make sure there is no whitespaces
     if (this.newTask.trim() !== '') {
-      this.tasks.push(this.newTask);
+      //Push new task into array with the name of the new task and its completion status set to false
+      this.tasks.push({ name: this.newTask, completed: false });
       this.newTask = ''; //Reset newTask after adding task
     }
+  }
+
+  //Method to mark a task as completed or not
+  taskCompleted(event: any, task: {name: string, completed: boolean}){
+    //updates the status of text based on the checkbox 
+    task.completed=event.target.checked;
   }
 }
