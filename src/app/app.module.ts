5be69dd8f8d50a494f8eb23e7bd059c2e3c96557
JakubSzkup1 <91326317+JakubSzkup1@users.noreply.github.com>
2024-04-25 18:06:23 +0000

    Setting up hosting and more styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 // Import necessary modules and components
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
+import { RouteReuseStrategy } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { environment } from '../environments/environment';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { WeekCalendarComponent } from './pages/week-calendar/week-calendar.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDtxO5BL0S5GqFwAVX-A-mzfsY8Ohu_GLU",
+    authDomain: "studyapp-1e2b8.firebaseapp.com",
+    projectId: "studyapp-1e2b8",
+    storageBucket: "studyapp-1e2b8.appspot.com",
+    messagingSenderId: "950902363692",
+    appId: "1:950902363692:web:3344f959e84c620dbfe389",
+    measurementId: "G-HNZYEXJ0KG"
+  }
 };
+
